dda97f2b9dd8875cce7f35c182e3c2a505f9eccf
kishore-ganesh <kishore2912000@gmail.com>
2018-07-25 14:26:08 +0530

    Improved DB structure
    Signed-off-by: kishore-ganesh <kishore2912000@gmail.com>

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const sequelize = new Sequelize({
     storage: 'database.sqlite'
   });
 
+const Op=sequelize.Op;
+
 
 // const sequelize=new Sequelize('database', {
 //     dialect: 'sqlite',
@@ -27,11 +29,14 @@ sequelize.authenticate().then(()=>{console.log("success")}).catch((err)=>{
 
 users=[];
 
+
 app.use(express.static('public_static'))
 
 app.post('/users', (req, res)=>{
 
-    res.send(JSON.stringify(users));
+    userlist=getUserList();
+    console.log(userlist);
+    res.send(JSON.stringify(userlist));
 })
 
 function sendInbox(socket, userdb)
@@ -51,64 +56,149 @@ function defineModel(name)
         messages: Sequelize.STRING
     });
 }
+
+
+var messagesdb=defineModel("messagesdb");
+function returnMessages(from, to)
+{   
+    return new Promise((resolve, reject)=>{
+
+        
+        messagesdb.sync().then(()=>{
+
+            messagesdb.findAll(
+                {
+                    where: {
+        
+                        from:{
+                            [Op.or]:[from, to]
+        
+                        },
+                        to: {
+                            [Op.or]:[from, to]
+                        }
+        
+                    }
+        
+                }    
+                ).then((messages)=>{
+        
+                    console.log(messages);
+                    resolve(messages);
+                })
+        })
+        
+    })
+}
+
+function emitMessage(username, message)
+{
+    usersockets=users[username];
+    usersockets.forEach((user)=>{
+
+        user.emit('message', message);
+    })
+
+}
+
+function sendMessage(from, to, message)
+{
+    
+    //console.log(usersockets);
+    messageobj={items: [{
+        from: from,
+        to: to,
+        messages: message}]}
+
+    emitMessage(from, messageobj);
+    emitMessage(to, messageobj);
+
+
+
+
+    messagesdb.create({from: from, to: to, messages: message});
+
+
+}
+
+function getUserList()
+{
+    userlist=Object.keys(users);
+    processedUserList=[];
+    userlist.forEach((user)=>{
+
+        if(users[user].length>=1)
+        {
+            processedUserList.push(user);
+        }
+
+    })
+    return processedUserList;
+    //make this more efficient
+}
+
+
 io.on('connection', (socket)=>{
 
     var username;
+    var selectedUser;
     var userdb;
 
+    io.emit('updatelist');
     
     socket.on('initialize', (data)=>{
 
         console.log('initializing');
-        socket.join(data.username);
+       // socket.join(data.username);
         username=data.username;
-        users.push(username);
+        if(!users[username])
+        {
+            users[username]=[];
+        }
+
+        users[username].push(socket);
+        //users.push(username);
         //console.log(username.toString());
 
-        userdb=defineModel(username);
-
-        userdb.sync().then()
-        {
-            sendInbox(socket, userdb);
-
-        };
+        
         io.emit('updatelist');
+
 
 
         
     })
 
+    socket.on('userSelected', (data)=>{
+
+        selectedUser=data.selectedUser;
+        returnMessages(username, selectedUser).then((messages)=>{
+        
+            
+            socket.emit('message', {items:messages});
+        }
+    
+    );
+
+    })
+
     socket.on('send', (data)=>{
         console.log("sending")
-        to=data.to;
-        messageobj={
-            from: username,
-            to: to,
-            messages: data.message}
-        payload= {
-                
-            items: [messageobj]
-        };
-        io.to(to).emit('message', 
-           payload
-        );
-        io.to(username).emit('message', payload);
-        toUserDB=defineModel(to);
-        toUserDB.sync().then(()=>{
-
-            toUserDB.create(messageobj);
-            userdb.create(messageobj);
-        })
-
-        
-
+      //  to=data.to;
+       
+        sendMessage(username, selectedUser, data.message);
     })
 
     socket.on('disconnect', ()=>{
 
         console.log("Disconnecting from room")
-        socket.leave(username);
-        users.splice(users.indexOf(username),1);
+        //socket.leave(username);
+      //  console.log(users[username]);
+        if(users[username])
+        {
+            users[username].splice(0,1);
+        }
+        
+        //users.splice(users.indexOf(username),1);
         io.emit('updatelist');
         
     })
--- public_static/main.js
+++ public_static/main.js
@@ -1,32 +1,59 @@
 function refreshUserList()
 {
-    $.post('/users', (JSONdata)=>{
-        data=JSON.parse(JSONdata);
-        $("#userlist").html("");
-        dataset=new Set(data);
-        dataset.forEach((user)=>{
-            
-            $("#userlist").append($("<li>").html(user));
+    return new Promise((resolve, reject)=>{
 
+        $.post('/users', (JSONdata)=>{
+            data=JSON.parse(JSONdata);
+            $("#userlist").html("");
+            dataset=new Set(data);
+            dataset.forEach((user)=>{
+                
+                $("#userlist").append($("<li>", {class: "userlistitem"}).html(user));
+    
+            })
+
+          //  console.log("reachedEnd");
+            
+            resolve();
+    
+            
         })
     })
+    
 }
 
 window.onload=function()
 {   
     var socket=io();
+    var username;
+    var selectedUser;
+
+    
 
     $("#adduser").click(()=>
         {
-          socket.emit('initialize', {username: $("#username").val()})
-          refreshUserList();  
-          $("#forSending").toggleClass("hidden");
+          username=$("#username").val();
+          socket.emit('initialize', {username: username});
           $("#registration").toggleClass("hidden");
         })
+
+    $("#userlist").on('click', ".userlistitem", (e)=>{
+
+        
+                
+            console.log("clicked")
+            selectedUser=e.currentTarget.innerHTML;
+            console.log(selectedUser);
+            socket.emit('userSelected', {selectedUser: selectedUser});
+            $("#forSending").toggleClass("hidden");
+            
+        
+    })
+
+   
     $("#sendmessage").click(
         ()=>{
             socket.emit('send', {
-                to: $("#to").val(),
                 message: $("#message").val()
             })
         }
@@ -34,12 +61,15 @@ window.onload=function()
 
     socket.on('message', (data)=>{
 
+        console.log(data);
         items=data.items;
         console.log(data)
         console.log("recieved")
         items.forEach((item)=>{
             //console.log(item..message)
-            $("#inbox").append($('<div>', {class: "messageitem"}).html("From: "+item.from+"<br>"+"To: "+item.to+"<br>"+item.messages));
+            var attr;
+            
+            $("#inbox").append($('<div>', {class:"messageitem"}).html("<h3>"+item.from+"</h3>"+item.messages));
         })
        
 
@@ -48,7 +78,8 @@ window.onload=function()
   socket.on('updatelist', ()=>{
 
     refreshUserList();
-  }) 
+
+  });
   
     
 
